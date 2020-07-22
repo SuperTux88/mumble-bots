0f2fa4a7be7043945c847dd34b4b58e797208249
Benjamin Neff <benjamin@coding4coffee.ch>
2020-07-22 03:54:05 +0200

    Refactor kamin-bot and unify with other bots

--- mumble-kamin.rb
+++ mumble-kamin.rb
@@ -1,32 +1,40 @@
 #!/usr/bin/env ruby
- 
+# frozen_string_literal: true
+
 require "mumble-ruby"
-require 'rubygems'
-require 'thread'
- 
+require "eventmachine"
+
+CONFIG = {
+  mpd: {
+    fifo: ENV["MPD_FIFO"] || "/var/lib/mpd/tmp/kamin.fifo",
+  },
+  mumble: {
+    host: ENV["MUMBLE_HOST"] || "mumble.coding4.coffee",
+    port: (ENV["MUMBLE_PORT"] || "64738").to_i,
+    username: ENV["MUMBLE_USERNAME"] || "kamin-bot",
+  }
+}.freeze
+
 class MumbleMPD
- 
-	def initialize
-		@cli = Mumble::Client.new("mumble.coding4coffee.org", "64738", "kamin-bot", "")
-	end
- 
-	def start
-		@cli.connect
-		sleep(1)
-		#@cli.join_channel("kaminzimmer")
-		#sleep(1)
-		@cli.stream_raw_audio('/var/lib/mpd/tmp/kamin.fifo')
- 
-		begin
-			t = Thread.new do
-				gets
-			end
- 
-			t.join
-		rescue Interrupt => e
-		end
-	end
+  def initialize
+    @cli = Mumble::Client.new(
+      CONFIG[:mumble][:host],
+      CONFIG[:mumble][:port],
+      CONFIG[:mumble][:username],
+      ""
+    )
+  end
+
+  def start
+    @cli.connect
+    sleep(1)
+    #@cli.join_channel("kaminzimmer")
+    #sleep(1)
+    @cli.player.stream_named_pipe(CONFIG[:mpd][:fifo])
+  end
 end
- 
-client = MumbleMPD.new
-client.start
+
+EventMachine.run do
+  client = MumbleMPD.new
+  client.start
+end
