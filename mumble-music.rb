6f7e7fe7e9ab2bca256b18c348c1593d2e6d6f13
Benjamin Neff <benjamin@coding4coffee.ch>
2020-07-22 03:34:00 +0200

    Refactor music-bot and add env-var config

--- mumble-music.rb
+++ mumble-music.rb
@@ -1,51 +1,69 @@
 #!/usr/bin/env ruby
- 
+# frozen_string_literal: true
+
 require "mumble-ruby"
 require "eventmachine"
 require "ruby-mpd"
- 
+
+CONFIG = {
+  mpd: {
+    host: ENV["MPD_HOST"] || "localhost",
+    port: (ENV["MPD_PORT"] || "6600").to_i,
+    fifo: ENV["MPD_FIFO"] || "/var/lib/mpd/tmp/music.fifo",
+    playlist: ENV["MPD_PLAYLIST"] || "playlist"
+  },
+  mumble: {
+    host: ENV["MUMBLE_HOST"] || "mumble.coding4.coffee",
+    port: (ENV["MUMBLE_PORT"] || "64738").to_i,
+    username: ENV["MUMBLE_USERNAME"] || "music-bot",
+  }
+}.freeze
+
 class MumbleMPD
- 
-	def initialize
-		@mpd = MPD.new "localhost", 6600, callbacks: true
- 
-		@cli = Mumble::Client.new("mumble.coding4coffee.org", "64738", "music-bot", "")
-	end
- 
-	def start
-		@cli.connect
-		sleep(1)
-		@cli.player.stream_named_pipe('/var/lib/mpd/tmp/mpd.fifo')
+  def initialize  
+    @mpd = MPD.new(
+      CONFIG[:mpd][:host],
+      CONFIG[:mpd][:port],
+      callbacks: true
+    )
 
-		add_mpd_callbacks
+    @cli = Mumble::Client.new(
+      CONFIG[:mumble][:host],
+      CONFIG[:mumble][:port],
+      CONFIG[:mumble][:username],
+      ""
+    )
+  end
 
-		@mpd.connect
- 
-		if @mpd.stopped?
-			@mpd.play
-		end
-	end
- 
-	def add_mpd_callbacks
-		@mpd.on :playlistlength do |length|
-			if length < 2
-				playlist = MPD::Playlist.new @mpd, "playlist"
-				playlist.load
-			end
-		end
+  def start
+    @cli.connect
+    sleep(1)
+    @cli.player.stream_named_pipe(CONFIG[:mpd][:fifo])
 
-		@mpd.on :song do |song|
-			if !song.nil? && !(@sv_art == song.artist && @sv_alb == song.album && @sv_tit == song.title)
-				@sv_art = song.artist
-				@sv_alb = song.album
-				@sv_tit = song.title
-				@cli.text_channel(@cli.me.current_channel, "MPD: #{"#{song.artist} - " if song.artist}#{song.title}#{" (#{song.album})" if song.album}")
-			end
-		end
-	end
+    add_mpd_callbacks
+
+    @mpd.connect
+
+    @mpd.play if @mpd.stopped?
+  end
+
+  def add_mpd_callbacks
+    @mpd.on :playlistlength do |length|
+      MPD::Playlist.new(@mpd, CONFIG[:mpd][:playlist]).load if length < 2
+    end
+
+    @mpd.on :song do |song|
+      if !song.nil? && !(@sv_art == song.artist && @sv_alb == song.album && @sv_tit == song.title)
+        @sv_art = song.artist
+        @sv_alb = song.album
+        @sv_tit = song.title
+        @cli.text_channel(@cli.me.current_channel, "MPD: #{"#{song.artist} - " if song.artist}#{song.title}#{" (#{song.album})" if song.album}")
+      end
+    end
+  end
 end
 
 EventMachine.run do
-	client = MumbleMPD.new
-	client.start
+  client = MumbleMPD.new
+  client.start
 end
